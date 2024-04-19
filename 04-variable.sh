19909ba2e882a0158b1801589d3d47752a66c239
Asadi Divyalatha <divyalathaasadi@gmail.com>
2024-04-19 16:55:54 +0530

    variables

--- 04-variable.sh
+++ 04-variable.sh
@@ -3,4 +3,4 @@
 	Person2=$2
 	#Reffering the variables
 	echo "$Person1::Hi $Person2, How are you"
-     echo "$Person2::Hi $Person1. I'm doing fentastic,Thank you.How are you doing"
+    echo "$Person2::Hi $Person1. I'm doing fentastic,Thank you.How are you doing"
