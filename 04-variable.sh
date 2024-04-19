352802e784ea2a6e3ea99481e3cda696c25751e7
Asadi Divyalatha <divyalathaasadi@gmail.com>
2024-04-19 16:54:50 +0530

    variables

---
+++ 04-variable.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+	Person1=$1
+	Person2=$2
+	#Reffering the variables
+	echo "$Person1::Hi $Person2, How are you"
+     echo "$Person2::Hi $Person1. I'm doing fentastic,Thank you.How are you doing"
