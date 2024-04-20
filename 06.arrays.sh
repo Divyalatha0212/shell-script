06e3043c15de11fa513bd027d03e04a2b445c1f7
Asadi Divyalatha <divyalathaasadi@gmail.com>
2024-04-20 10:05:45 +0530

    arrays

--- 06.arrays.sh
+++ 06.arrays.sh
@@ -3,10 +3,10 @@
 MOVIES=("RRR" "DJ" "SSC")
  
 
- echo "first movie name is: ${MOVIES[0]}"
+ echo "first movie name is: ${MOVIES[@]}"
 
- echo "second movie name is: ${MOVIES[1]}"
+ echo "second movie name is: ${MOVIES[2]}"
  
- echo "third movie name is: ${MOVIES[2]}"
+ echo "third movie name is: ${MOVIES[1]}"
 
- echo "print all movie names are: ${MOVIES[@]}"
+ echo "print all movie names are: ${MOVIES[0]}"
