6c8655492b1a201fda4d20e26b1d453f7d45397a
Asadi Divyalatha <divyalathaasadi@gmail.com>
2024-04-20 10:01:34 +0530

    arrays

---
+++ 06.arrays.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+MOVIES=("RRR" "DJ" "SSC")
+ 
+
+ echo "first movie name is:${MOVIE[0]}"
+
+ echo "second movie name is:${MOVIE[1]}"
+ 
+ echo "third movie name is:${MoVIE[2]}"
+
+ echo "print all movie names are:${MOVIE[@]}"
