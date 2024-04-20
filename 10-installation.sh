530c0e3a51dfb4dc2109ed121a1fa970ace99af5
Asadi Divyalatha <divyalathaasadi@gmail.com>
2024-04-20 13:23:39 +0530

    mysql installation

--- 10-installation.sh
+++ 10-installation.sh
@@ -4,9 +4,27 @@ USERID=$(id-u)
 if [$USERID -nq 0]
 then
 echo "please run this script with root access"
+exit 1
 else
 echo "you are the super user"
-
 fi
 
 dnf install mysql -y
+
+if[$? -nq 0]
+ echo "Installation of MYSQL----FAILURE"
+
+ exit 1
+
+ else
+ echo "Installation of MYSQL-----SUCCESS"
+ fi
+
+ dnf install git -y
+
+ if [$? -nq 0]
+ echo "Installation of git---FAILURE"
+ exit 1
+ else
+ echo "Installation of git----SUCCESS"
+ fi
