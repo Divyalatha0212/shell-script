8267172e66c07f6329e2493541bd682d39bd4948
Asadi Divyalatha <divyalathaasadi@gmail.com>
2024-04-21 19:30:12 +0530

    functions

--- 11-functions.sh
+++ 11-functions.sh
@@ -2,13 +2,14 @@
 USERID=$(id -u)
 
 VALIDATE(){
-if [ $1 -ne 0 ]
-then
-echo "$2...FAILURE"
-exit 1
-else 
-echo "$2...SUCCESS"
-fi
+
+       if [ $1 -ne 0 ]
+       then
+           echo "$2...FAILURE"
+           exit 1
+        else 
+           echo "$2...SUCCESS"
+       fi
 }
 
 if [ $USERID -ne 0 ]
