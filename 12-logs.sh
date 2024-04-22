fe476c5ede5cc91216edab64ad2b0e2ea9002531
Asadi Divyalatha <divyalathaasadi@gmail.com>
2024-04-22 12:47:16 +0530

    logs

--- 12-logs.sh
+++ 12-logs.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPT_NAME=$(echo $0 | cut -d "." F1)
-LOGFILE=/tmp/$SCRIPTNAME-$TIMESTAMP.log
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 
 
 
