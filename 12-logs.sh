0282110212b31cbe82081be0a82dbd7993cfd314
Asadi Divyalatha <divyalathaasadi@gmail.com>
2024-04-22 11:27:22 +0530

    logs

---
+++ 12-logs.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+USERID=$(id -u)
+TIMESTAMP=($date +%F-%H-%M-%S)
+
+VALIDATE(){
+
+        if [ $1 -ne 0 ]
+   then
+        echo "$2...FAILURE"
+        exit 1
+    else
+        echo "$2...SUCCESS"
+    fi
+}
+
+        if [ $USERID -ne 0 ]
+then
+        echo " please run this in root "
+        exit 1
+else
+        echo "you are the root user"
+ fi
+
+dnf install mysql -y
+VALIDATE $? "Installing MYSQL"
+
+dnf install git -y
+VALIDATE $? "Installing Git"
