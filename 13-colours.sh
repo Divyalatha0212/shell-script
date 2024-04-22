4532b74ff245d89824eac4658b44a0412250939d
Asadi Divyalatha <divyalathaasadi@gmail.com>
2024-04-22 13:39:29 +0530

    colours

--- 13-colours.sh
+++ 13-colours.sh
@@ -34,5 +34,5 @@ VALIDATE $? "Installing MySQL"
 dnf install git -y &>>$LOGFILE
 VALIDATE $? "Installing Git"
 
-dnf install nginx -y &>>$LOGFILE
-VALIDATE $? "Installing nginx"
+dnf install docker -y &>>$LOGFILE
+VALIDATE $? "Installing docker"
