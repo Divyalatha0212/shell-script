48f697610ef734265a80ad3fe884d20e651b6d97
Asadi Divyalatha <divyalathaasadi@gmail.com>
2024-04-22 10:55:07 +0530

    functions

--- 11-functions.sh
+++ 11-functions.sh
@@ -3,21 +3,21 @@ USERID=$(id -u)
 
 VALIDATE(){
 
-       if [ $1 -ne 0 ]
-       then
-           echo "$2...FAILURE"
-           exit 1
-        else 
-           echo "$2...SUCCESS"
-       fi
+        if [ $1 -ne 0 ]
+   then
+        echo "$2...FAILURE"
+        exit 1
+    else
+        echo "$2...SUCCESS"
+    fi
 }
 
-if [ $USERID -ne 0 ]
+        if [ $USERID -ne 0 ]
 then
-echo " please run this in root "
-exit 1
+        echo " please run this in root "
+        exit 1
 else
-echo "you are the root user"
+        echo "you are the root user"
  fi
 
 dnf install mysql -y
