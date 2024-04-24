54fc2b5cffa975d52073adf9586a74ce38baf91a
Asadi Divyalatha <divyalathaasadi@gmail.com>
2024-04-24 12:21:42 +0530

    installpackages

--- 15-installpackages.sh
+++ 15-installpackages.sh
@@ -1,42 +1,16 @@
 #!/bin/bash
 
 USERID=&(id -u) #ROOT USER
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1) #SCRIPT NAME
-TIMESTAMP=$(date +%F-%H-%M-%S) #IT GIVES THE TIME 
-#LOGFILE=/tmp/$SCRIPT_NAME/$TIMESTAMP.log #creating the temporary directory and svae the logfiles into it.
-LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
-
-R="\e[31m"
-G="\e[32m"
-N="\e[0m"
-
-echo "script started executing at $TIMESTAMP"
 
 if [ $USERID -ne 0 ]
 then
-    echo "please run the script with root access."
-    exit 1
-else
-    echo "your the root user"
-
-fi  
-
-
-VALIDATE(){
-
-if [ $1 -ne 0 ]
-then
-    echo -e "$2 ---$R FAILURE $N"
+echo "run this script with root user."
 exit 1
 else
-     echo -e "$2---$G SUCCESS $N" 
-
+echo "you are the root user."
 fi
-}
+
+echo "all packages are: $@"
 
 
-dnf install mysql -y &>>$LOGFILE
-VALIDATE $? "installing the MYSQL"
 
-dnf  install docker -y
-VALIDATE $? "installing the DOCKER" &>>$LOGFILE
