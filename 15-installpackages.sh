5d230685129e852a4bdff6ed8adc827de6f6b3cf
Asadi Divyalatha <divyalathaasadi@gmail.com>
2024-04-24 12:44:42 +0530

    installpackages

--- 15-installpackages.sh
+++ 15-installpackages.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 USERID=$(id -u) #ROOT USER
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILES=/tmp/$SCRIPTNAME-$TIMESTAMP.log
 
 if [ $USERID -ne 0 ]
 then
@@ -13,6 +16,12 @@ fi
 for i in $@
 do
 echo "packages to install: $i"
+dnf instaaled  $i &>>$LOGFILES
+if [$? -eq o]
+then
+echo "$i already installed---SKIPPING"
+fi
+
 done
 
 
